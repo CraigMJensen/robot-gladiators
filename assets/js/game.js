f0f20afb93ee9dea6a52f95b774d32dc058abe0a
CraigJensen <cmjensen82@gmail.com>
2021-11-12 16:56:32 -0600

    add js to learn console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// Note lack of quotation marks around playerName
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// This will do math and log 20
+console.log(10 + 10);
+// What is this about?  This is string concatenation.  combines strings and variables among others.
+console.log("Our robot's name is " + playerName);
+// This creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
