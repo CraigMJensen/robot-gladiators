e2167d06a30c2da854c06d56e2ffde4fe4ef25bb
CraigJensen <cmjensen82@gmail.com>
2021-11-12 17:22:06 -0600

    first round of the fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,29 @@
 var playerName = window.prompt("What is your robot's name?");
-// Note lack of quotation marks around playerName
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-// This will do math and log 20
-console.log(10 + 10);
-// What is this about?  This is string concatenation.  combines strings and variables among others.
-console.log("Our robot's name is " + playerName);
-// This creates a function named "fight"
-function fight() {
-    window.alert("The fight has begun!");
-}
+var playerHealth = 100;
+var playerAtack = 10;
 
-// fight();
+// You can log multiple values at once, example console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    // Alert players that the round is starting
+    window.alert("Welcome to Robot Gladiators!");
+
+    // Subtract the value of "playerAttack" from the value of "enemyHealth" and use that result to update the value in the "enemyHealth" variable
+    enemyHealth = enemyHealth - playerAtack;
+    // Log a resulting message to the console so we know it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+    // Subtract value of "enemyAttack" from the value of "playerHealth" and use that result to update value in "playerHealth" variable
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console se we know it worked.
+    console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+};
+
+fight();
