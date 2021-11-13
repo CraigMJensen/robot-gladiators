55cb768eff90590a4f9f7ce0eb7b1d540005e861
CraigJensen <cmjensen82@gmail.com>
2021-11-13 06:51:12 -0600

    one complete round of the fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,12 +18,28 @@ var fight = function() {
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
+
+    // Check enemy's health
+    if (enemyHealth <=0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
     // Subtract value of "enemyAttack" from the value of "playerHealth" and use that result to update value in "playerHealth" variable
     playerHealth = playerHealth - enemyAttack;
     // Log a resulting message to the console se we know it worked.
     console.log(
     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
+
+    // check player's health
+    if (playerHealth <=0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 };
 
 fight();
