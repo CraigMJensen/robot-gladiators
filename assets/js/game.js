85648a620976036e0e455455a87a6badf853d928
CraigJensen <cmjensen82@gmail.com>
2021-11-13 18:37:00 -0600

    enemyNames array

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,11 +3,17 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roborto";
+var enemyName = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+// Game States
+// "WIN" - Player robot has defeated all enemy robots
+//    * Fight all enemy robots
+//    * Defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
+
+var fight = function (enemyName) {
     window.alert("Welcome to Robot Gladiators!");
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -51,11 +57,12 @@ var fight = function () {
             fight();
         }
 
-        // window.alert(playerName + " has chosen to skip the fight!");
-    } // else {
-    // window.alert("You need to choose a valid option. Try again!");
-    // }
+    } else {
+        window.alert("You need to pick a valid option. Try again!");
+    }
 
 };
 
-fight();
+for (var i = 0; i < enemyName.length; i++) {
+    fight(enemyName[i]);
+}
