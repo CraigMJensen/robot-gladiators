4e2d4b074c5fe95fc8ffa1b109f7da58b77736e7
CraigJensen <cmjensen82@gmail.com>
2021-11-17 18:27:26 -0600

    fixed bug name prompt accepting null

--- assets/js/game.js
+++ assets/js/game.js
@@ -148,8 +148,19 @@ var randomNumber = function (min, max) {
     return value;
 };
 
+var getPlayerName = function () {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -194,5 +205,10 @@ var enemyInfo = [
         attack: randomNumber(10, 14)
     }
 ];
+
+console.log(enemyInfo);
+console.log(enemyInfo[0]);
+console.log(enemyInfo[0].name);
+console.log(enemyInfo[0]['attack']);
 
 startGame();
